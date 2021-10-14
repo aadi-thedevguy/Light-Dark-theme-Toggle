d89fbe49ede957ba031d7ccc5311fe5cb8ceeb20
aditya <adikhare24@gmail.com>
2021-10-14 20:43:30 +0530

    added custom theme selection

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 
+const headingText = document.querySelector('h2');
 const sunMoonContainer = document.querySelector('.sun-moon-container');
 const toggleButton = document.querySelector('.theme-toggle-button');
+
+setTimeout(customTheme,5000)
+ 
+ function customTheme() {
+        confirm('Did not like our Themes, Want to create your own ?')
+        let color = prompt('Just Type the Color you want','skyblue, gray, crimson')
+
+        if (color === null) return
+        document.body.style.backgroundColor = color
+        toggleButton.style.display = 'none'
+        headingText.innerHTML = `<span style="color:white;">Custom Theme is active now</span>`
+        
+ }
+
 
 
 document.querySelector('.theme-toggle-button').addEventListener('click', () => {
     document.body.classList.toggle('dark');
     toggleButton.classList.toggle('dark');
+    
     const currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'))
     sunMoonContainer.style.setProperty('--rotation',currentRotation + 180);
 })
